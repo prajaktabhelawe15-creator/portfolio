51910718a9019b6ec9bd9c082d344458a7d457c7
Prajakta <prajaktabhelawe12@gmail.com>
2026-05-29 07:36:28 +0530

    changes 1

--- portfolio-backend/src/main/java/com/portfolio/portfolio_backend/PortfolioBackendApplication.java
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/PortfolioBackendApplication.java
@@ -4,10 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@SpringBootApplication- shortcut for 3 annotations:
+//	@Configuration- this class can define Spring beans
+//	@EnableAutoConfiguration- Spring Boot auto-configures based on your classpath
+//	@CoponentScan- scans this package and all sub-packages for @Controller, @Service, @Repository, @Component
+
 public class PortfolioBackendApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PortfolioBackendApplication.class, args);
+		//SpringApplication.run() bootstraps the entire application:
+		//1.Creates the Spring ApplicationContext (the IoC container)
+		//2.Starts the embedded Tomcat server
+		//3.Scans and registers all beans
+		//4.Sets up th;e database connection pool
+
 	}
 
 }
---
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/model/ContactMessage.java
@@ -0,0 +1,4 @@
+package com.portfolio.portfolio_backend.model;
+
+public class ContactMessage {
+}
